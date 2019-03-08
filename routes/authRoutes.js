62ca2710d3ea09ec39b464771dd41ea8a81f389f
Zhuo Chen <chen519@usc.edu>
2019-03-07 16:47:38 -0800

    -

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-
+const CircularJSON = require("circular-json");
 module.exports = app => {
   app.get(
     "/auth/google",
@@ -23,6 +23,6 @@ module.exports = app => {
   });
 
   app.get("/api/current_user", (req, res) => {
-    res.send(req.user);
+    res.send(CircularJSON.stringify(req));
   });
 };
