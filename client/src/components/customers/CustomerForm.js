6e8cf048e568de15a24fe68ead4998ca6bd16ea2
Zhuo Chen <chen519@usc.edu>
2019-03-08 14:48:21 -0800

    material ui

--- client/src/components/customers/CustomerForm.js
+++ client/src/components/customers/CustomerForm.js
@@ -3,8 +3,21 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import formFields from "./formFields";
 
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import MenuItem from "material-ui/MenuItem";
+import { RadioButton } from "material-ui/RadioButton";
+import {
+  //Checkbox,
+  RadioButtonGroup,
+  SelectField,
+  TextField,
+  //Toggle,
+  DatePicker
+} from "redux-form-material-ui";
+
 class CustomerForm extends React.Component {
-  renderError({ error, touched }) {
+  renderError({ touched, error }) {
     if (touched && error) {
       return (
         <div className="ui error message">
@@ -19,12 +32,8 @@ class CustomerForm extends React.Component {
 
     return (
       <div className={className}>
-        <label>{label}</label>
-        <input
-          {...input}
-          autoComplete="off"
-          type={tp === "date" ? "date" : "text"}
-        />
+        {tp === "radio" ? "" : <label>{label}</label>}
+        <input {...input} autoComplete="off" type={tp} />
         {this.renderError(meta)}
       </div>
     );
@@ -36,8 +45,158 @@ class CustomerForm extends React.Component {
     this.props.onSubmit(formValues);
   };
 
+  renderFields2 = () => {
+    return (
+      <React.Fragment>
+        <div>
+          <label>{formFields.firstName.label}</label>
+          <Field
+            name="firstName"
+            component={({ input, meta }) => (
+              <div>
+                <input
+                  type="text"
+                  {...input}
+                  autoComplete="off"
+                  placeholder="First Name"
+                />
+                {this.renderError(meta)}
+              </div>
+            )}
+          />
+        </div>
+        <div>
+          <label>{formFields.lastName.label}</label>
+          <Field
+            name="lastName"
+            component={({ input, meta }) => (
+              <div>
+                <input
+                  {...input}
+                  autoComplete="off"
+                  type="text"
+                  placeholder="Last Name"
+                />
+                {this.renderError(meta)}
+              </div>
+            )}
+          />
+        </div>
+        <div>
+          <label>{formFields.email.label}</label>
+          <Field
+            name="email"
+            component={({ input, meta }) => (
+              <div>
+                <input
+                  type="email"
+                  {...input}
+                  autoComplete="off"
+                  placeholder="Email"
+                />
+                {this.renderError(meta)}
+              </div>
+            )}
+          />
+        </div>
+        <div>
+          <label>{formFields.sex.label}</label>
+          <div>
+            <label>
+              <Field
+                name="sex"
+                component={({ input, meta }) => (
+                  <div>
+                    <input type="radio" {...input} value="Male" />
+                    {this.renderError(meta)}
+                  </div>
+                )}
+              />
+              Male
+            </label>
+            <label>
+              <Field
+                name="sex"
+                component={({ input, meta }) => (
+                  <div>
+                    <input type="radio" {...input} value="Female" />
+                    {this.renderError(meta)}
+                  </div>
+                )}
+              />
+              Female
+            </label>
+          </div>
+        </div>
+        <div>
+          <label>{formFields.age.label}</label>
+          <Field
+            name="age"
+            component={({ input, meta }) => (
+              <div>
+                <input
+                  autoComplete="off"
+                  type="number"
+                  {...input}
+                  placeholder="Age"
+                />
+                {this.renderError(meta)}
+              </div>
+            )}
+          />
+        </div>
+        <div>
+          <label>{formFields.position.label}</label>
+          <Field
+            name="position"
+            component={({ input, meta }) => (
+              <div>
+                <select {...input}>
+                  <option />
+                  <option value="ff0000">Red</option>
+                  <option value="00ff00">Green</option>
+                  <option value="0000ff">Blue</option>
+                </select>
+                {this.renderError(meta)}
+              </div>
+            )}
+          />
+        </div>
+      </React.Fragment>
+    );
+  };
+
   renderFields = () => {
     return _.map(formFields, ({ label, name, type }) => {
+      if (type === "radio") {
+        return (
+          <div>
+            <label>Sex!!</label>
+            <div>
+              <label>
+                <Field
+                  name="sex"
+                  component={this.renderInput}
+                  type="radio"
+                  value="male"
+                  tp={type}
+                />{" "}
+                Male
+              </label>
+              <label>
+                <Field
+                  name="sex"
+                  component={this.renderInput}
+                  type="radio"
+                  value="female"
+                  tp={type}
+                />{" "}
+                Female
+              </label>
+            </div>
+          </div>
+        );
+      }
       return (
         <Field
           key={name}
@@ -50,7 +209,146 @@ class CustomerForm extends React.Component {
     });
   };
 
-  renderForm = () => {};
+  renderFields3 = () => {
+    return (
+      <React.Fragment>
+        <MuiThemeProvider>
+          <div>
+            <Field
+              name="firstName"
+              component={TextField}
+              hintText="First Name"
+              fullWidth
+              floatingLabelText="First Name"
+              value="asdsad"
+            />
+            <Field
+              name="lastName"
+              component={TextField}
+              hintText="Last Name"
+              fullWidth
+              floatingLabelText="Last Name"
+            />
+
+            <br />
+            <br />
+
+            <label>Sex</label>
+            <Field
+              name="sex"
+              component={this.renderRadioButtonGroup}
+              floatingLabelText="Last Name"
+              value={"Male"}
+            >
+              <RadioButton value="Male" label="Male" />
+              <RadioButton value="Female" label="Famale" />
+            </Field>
+
+            <Field
+              name="age"
+              component={TextField}
+              hintText="Age"
+              fullWidth
+              floatingLabelText="Age"
+            />
+            <Field
+              name="email"
+              component={TextField}
+              hintText="Email"
+              fullWidth
+              floatingLabelText="Email"
+            />
+            <Field
+              name="position"
+              component={
+                this.props.initialValues || false
+                  ? this.renderSelectField
+                  : SelectField
+              }
+              floatingLabelText="Position"
+              value="Manager"
+            >
+              <MenuItem value="Employee" primaryText="Employee" />
+              <MenuItem value="Manager" primaryText="Manager" />
+              <MenuItem value="Senior Manager" primaryText="Senior Manager" />
+            </Field>
+
+            <Field
+              name="joinedTime"
+              component={
+                this.props.initialValues || false
+                  ? this.renderDatePicker
+                  : DatePicker
+              }
+              format={null}
+              hintText="When did he/she join in?"
+            />
+
+            <Field
+              name="description"
+              component={TextField}
+              hintText="Describe him/her"
+              floatingLabelText="Description"
+              multiLine={true}
+              rows={3}
+            />
+
+            {/* <Field
+            name="agreeToTerms"
+            component={Checkbox}
+            label="Agree to terms?"
+          /> */}
+
+            {/* <Field
+            name="receiveEmails"
+            component={Toggle}
+            label="Please spam me!"
+          /> */}
+          </div>
+        </MuiThemeProvider>
+      </React.Fragment>
+    );
+  };
+
+  renderDatePicker = props => {
+    const date = new Date(this.props.initialValues.joinedTime);
+    if (!props.meta.dirty) props.input.value = date || null;
+
+    return (
+      <div>
+        <DatePicker {...props} />
+      </div>
+    );
+  };
+
+  renderTextField = props => {
+    return (
+      <div>
+        <TextField {...props} />
+      </div>
+    );
+  };
+
+  renderSelectField = props => {
+    if (props.meta.invalid)
+      props.input.value = this.props.initialValues.position;
+    return (
+      <div>
+        <SelectField value={props.value} {...props} />
+      </div>
+    );
+  };
+
+  renderRadioButtonGroup = props => {
+    if ((this.props.initialValues || false) && props.meta.invalid)
+      props.input.value = this.props.initialValues.sex;
+    return (
+      <div>
+        <RadioButtonGroup {...props} />
+        {this.renderError(props.meta)}
+      </div>
+    );
+  };
 
   render() {
     return (
@@ -58,8 +356,23 @@ class CustomerForm extends React.Component {
         className="ui form error" // error to show error message
         onSubmit={this.props.handleSubmit(this.onSumbit)}
       >
-        {this.renderFields()}
-        <button className="ui button primary">Submit</button>
+        {this.renderFields3()}
+        <button
+          disabled={
+            !(this.props.initialValues || false) &&
+            (this.props.pristine || this.props.submitting)
+          }
+          className="ui button primary"
+        >
+          Submit
+        </button>
+        <button
+          disabled={this.props.pristine || this.props.submitting}
+          className="ui button secondary"
+          onClick={this.props.reset}
+        >
+          {this.props.initialValues || false ? "Reset" : "Clear"}
+        </button>
       </form>
     );
   }
@@ -80,8 +393,8 @@ const validate = formValues => {
   if (!formValues.sex) {
     errors.sex = "Sex Required";
   }
-  if (!formValues.favoriteColor) {
-    errors.favoriteColor = "Favorite Color Required";
+  if (!formValues.position) {
+    errors.position = "Position Required";
   }
   if (!formValues.email) {
     errors.email = "Email Required";
--- client/src/components/customers/formFields.js
+++ client/src/components/customers/formFields.js
@@ -1,58 +1,58 @@
-export default [
-  {
+export default {
+  firstName: {
     label: "First Name",
     name: "firstName",
     type: "text",
     values: [],
     noValueError: "You must provide your First Name"
   },
-  {
+  lastName: {
     label: "Last Name",
     name: "lastName",
     type: "text",
     values: [],
     noValueError: "You must provide your Last Name"
   },
-  {
+  sex: {
     label: "Sex",
     name: "sex",
     type: "radio",
     noValueError: "You must provide your Sex",
     values: ["Male", "Female"]
   },
-  {
+  age: {
     label: "Age",
     name: "age",
     type: "text",
     values: [],
     noValueError: "You must provide your Age"
   },
-  {
+  email: {
     label: "Email",
     name: "email",
     type: "email",
     values: [],
     noValueError: "You must provide an Email"
   },
-  {
+  description: {
     label: "Description",
     name: "description",
     type: "textarea",
     values: [],
     noValueError: "You must provide a Description"
   },
-  {
+  position: {
     label: "Pisition",
     name: "position",
     type: "select",
     values: ["Manager, Employee, Senior Manager"],
     noValueError: "You must provide a Pisition"
   },
-  {
+  joinedTime: {
     label: "Joined Time",
     name: "joinedTime",
     type: "date",
     values: [],
     noValueError: "You must provide a Joined Time"
   }
-];
+};
